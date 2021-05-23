bb610ac5f849adc3d4432b57623822ae5dbe7cd3
Tilzen <lucas.nascimentoR@hotmail.com>
2021-05-23 14:48:23 -0300

    feat: Implementação de captura de eventos de trade da binance via ws.

---
+++ apps/streamer/lib/streamer/binance.ex
@@ -0,0 +1,38 @@
+defmodule Streamer.Binance do
+  use WebSockex
+
+  @stream_endpoint "wss://stream.binance.com:9443/ws/"
+
+  def start_link(symbol, state) do
+    symbol = String.downcase(symbol)
+    url = "#{@stream_endpoint}#{symbol}@trade"
+
+    WebSockex.start_link(url, __MODULE__, state)
+  end
+
+  def handle_frame({_type, message}, state) do
+    case Jason.decode(message) do
+      {:ok, event} -> handle_event(event, state)
+      {:error, _} -> throw("Unable to decode message: #{message}")
+    end
+
+    {:ok, state}
+  end
+
+  def handle_event(%{"e" => "trade"} = event, _state) do
+    trade_event = %Streamer.Binance.TradeEvent{
+      :event_type => event["e"],
+      :event_time => event["E"],
+      :symbol => event["s"],
+      :trade_id => event["t"],
+      :price => event["p"],
+      :quantity => event["q"],
+      :buyer_order_id => event["b"],
+      :seller_order_id => event["a"],
+      :trade_time => event["T"],
+      :is_buyer_market_maker? => event["m"]
+    }
+
+    IO.inspect trade_event, label: "Trade event received :"
+  end
+end
