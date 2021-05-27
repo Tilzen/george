f54f2e727986ddcd92e8bc17c7552668dfec03ca
Tilzen <lucas.nascimentoR@hotmail.com>
2021-05-27 12:07:33 -0300

    chore: Configura childrens para supervisão.

--- apps/greek/lib/greek/application.ex
+++ apps/greek/lib/greek/application.ex
@@ -8,8 +8,11 @@ defmodule Greek.Application do
   @impl true
   def start(_type, _args) do
     children = [
-      # Starts a worker by calling: Greek.Worker.start_link(arg)
-      # {Greek.Worker, arg}
+      {
+        DynamicSupervisor,
+        strategy: :one_for_one, name: Greek.DynamicSupervisor
+      },
+      {Greek.Server, []}
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
