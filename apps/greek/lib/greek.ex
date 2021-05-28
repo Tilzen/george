c92e0746c66d2544b4c90de8556dcb3b5343fae3
Tilzen <lucas.nascimentoR@hotmail.com>
2021-05-28 20:52:27 -0300

    refact: Remove send_event de greek.

--- apps/greek/lib/greek.ex
+++ apps/greek/lib/greek.ex
@@ -2,8 +2,4 @@ defmodule Greek do
   @moduledoc """
   Documentation for `Greek`.
   """
-
-  def send_event(%Streamer.Binance.TradeEvent{} = event) do
-    GenServer.cast(:trader, {:event, event})
-  end
 end
