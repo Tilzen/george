4a09be6ce398e8f4f2c0b4933cafe0559ae94452
Tilzen <lucas.nascimentoR@hotmail.com>
2021-06-03 15:39:51 -0300

    chore: Adiciona moduledoc em Greek.

--- apps/greek/lib/greek.ex
+++ apps/greek/lib/greek.ex
@@ -1,5 +1,9 @@
 defmodule Greek do
   @moduledoc """
-  Documentation for `Greek`.
+  App dedicated to the description of trading strategies.
+
+  "The concept of strategy, in Greek strateegia, in Latin strategi, in French
+  stratégie, in English strategy, in German strategie, in Italian strategia
+  in Spanish estrategia."
   """
 end
